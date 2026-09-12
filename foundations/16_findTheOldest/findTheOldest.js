53ebe06cb253c670b48639ee8f713825d74e771c
karthikcodes090905 <kc5182@srmist.edu.in>
2026-09-12 18:43:22 +0530

    completed these assignment ques

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+  let oldest = people.reduce((oldest, current) =>{
+   let currentAge = (current.yearOfDeath || new Date().getFullYear()) - current.yearOfBirth ;
+   let oldestAge = (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth ;
+   if(currentAge > oldestAge) return current ;
+   else return oldest ;
+  } , people[0]);
+
+ return oldest ;
+
+
+
+
+    /*
+ let age = people.map((person) =>{
+    let born = person.yearOfBirth ;
+    let die = person.yearOfDeath ;
+    return age  = die - born ; 
+ });
+
+ let oldest = Math.max(...age);
+ return oldest ;
+ */
 };
 
 // Do not edit below this line
